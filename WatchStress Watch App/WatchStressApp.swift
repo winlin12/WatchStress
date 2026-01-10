7d2d8ddc143525413a0b9529836480c78eadeaea
winlin12 <winlin12345678@gmail.com>
2026-01-10 10:39:42 -0500

    Initial Commit

---
+++ WatchStress Watch App/WatchStressApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchStressApp.swift
+//  WatchStress Watch App
+//
+//  Created by Winston Lin on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchStress_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchStress/WatchStressApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchStressApp.swift
+//  WatchStress
+//
+//  Created by Winston Lin on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchStressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
